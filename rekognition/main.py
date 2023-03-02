24556d63e5a944ab64de5c5acfa553058e093574
Cjwong314 <cjwong1234@gmail.com>
2023-03-01 23:06:16 -0700

    3/1

--- rekognition/main.py
+++ rekognition/main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import boto3
+import json
 
 starttime = time.time()
 
@@ -23,64 +24,56 @@ def take_photo(pic,  port=0, ramp_frames=30, x=1280, y=720):
 
 def upload_photo(pic):
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file('/Users/cjwong/Desktop/solving_school_shootings/'+str(pic), 'gun-rekognition', str(pic))
+    # s3.meta.client.upload_file('/Users/cjwong/Desktop/solving_school_shootings/'+str(pic), 'gun-rekognition', str(pic))
+    s3.meta.client.upload_file(str(pic), 'gun-rekognition', str(pic))
     print("photo uploaded")
 
 def detect_labels(pic, bucket):
 
-     session = boto3.Session()
-     client = session.client('rekognition')
+    session = boto3.Session()
+    client = session.client('rekognition')
 
-     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':pic}},
-     MaxLabels=10,
-     # Uncomment to use image properties and filtration settings
-     #Features=["GENERAL_LABELS", "IMAGE_PROPERTIES"],
-     #Settings={"GeneralLabels": {"LabelInclusionFilters":["Cat"]},
-     # "ImageProperties": {"MaxDominantColors":10}}
-     )
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':pic}},
+    MaxLabels=10)
 
-     print('Detected labels for ' + pic)
-     print()
-     for label in response['Labels']:
-         print("Label: " + label['Name'])
-         print("Confidence: " + str(label['Confidence']))
-         print("Instances:")
+    print('Detected labels for ' + pic)
+    print()
+    for label in response['Labels']:
+        print("Label: " + label['Name'])
+        print("Confidence: " + str(label['Confidence']))
+        print("Instances:")
 
-         for instance in label['Instances']:
-             print(" Bounding box")
-             print(" Top: " + str(instance['BoundingBox']['Top']))
-             print(" Left: " + str(instance['BoundingBox']['Left']))
-             print(" Width: " + str(instance['BoundingBox']['Width']))
-             print(" Height: " + str(instance['BoundingBox']['Height']))
-             print(" Confidence: " + str(instance['Confidence']))
-             print()
+        if label['Name'] == "Gun":
+            print("You dead")
+            exit()
 
-         print("Parents:")
-         for parent in label['Parents']:
+        print("Parents:")
+        for parent in label['Parents']:
             print(" " + parent['Name'])
 
-         print("Aliases:")
-         for alias in label['Aliases']:
-             print(" " + alias['Name'])
+        print("Aliases:")
+        for alias in label['Aliases']:
+            print(" " + alias['Name'])
 
-             print("Categories:")
-         for category in label['Categories']:
-             print(" " + category['Name'])
-             print("----------")
-             print()
+            print("Categories:")
+        for category in label['Categories']:
+            print(" " + category['Name'])
+            print("----------")
+            print()
 
-     if "ImageProperties" in str(response):
-         print("Background:")
-         print(response["ImageProperties"]["Background"])
-         print()
-         print("Foreground:")
-         print(response["ImageProperties"]["Foreground"])
-         print()
-         print("Quality:")
-         print(response["ImageProperties"]["Quality"])
-         print()
+    if "ImageProperties" in str(response):
+        print("Background:")
+        print(response["ImageProperties"]["Background"])
+        print()
+        print("Foreground:")
+        print(response["ImageProperties"]["Foreground"])
+        print()
+        print("Quality:")
+        print(response["ImageProperties"]["Quality"])
+        print()
 
-     return len(response['Labels'])
+    return len(response['Labels'])
+
 
 def main():
     current_time = time.ctime()
@@ -91,7 +84,10 @@ def main():
     label_count = detect_labels(pic, bucket)
     print("Labels detected: " + str(label_count))
     
+    
+    
 
 while (True): 
     main()
+
     time.sleep(15 - ((time.time() - starttime) % 15))
